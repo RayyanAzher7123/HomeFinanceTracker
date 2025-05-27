57f1b60f0fc818d9e9afe3e45e69d1624d0c4cf9
Miswani <Rayyan.Miswani@Sobeys.com>
2025-05-27 09:53:26 -0400

    Initial commit - Home Finance Tracker MVC App

---
+++ HomeFinance.web/Models/Expense.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeFinance.web.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+
+    }
+}
